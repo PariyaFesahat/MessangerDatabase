c9cfe7bbe424a72f6be707923e7c9214aef06679
Iliya.mi <iliya.mi78@gmail.com>
2021-06-21 20:14:20 +0430

    Chat Procedure in process - 2 Materialized View/Function Done

--- ProcedureQueries.sql
+++ ProcedureQueries.sql
@@ -5,34 +5,49 @@ USE Elmos_Messanger
 --Where Picture Is Not Null
 /*========================================================== [PROCEDURES] ===================================================*/
 
---A View for users whenever they get into a chat (PV/Group/Channel) - Used For |Open_Chat_Proc| Procedure
+-- Materialized View for users whenever they get into a chat (PV/Group/Channel) - Used For |Open_Chat_Proc| Procedure
 Create Or Alter View Message_Details
 With Schemabinding
 As
 Select
+	m.txt, m.files, m.music, m.video, m.Picture, m.locations, m.times, m.dates,
 	c.F_Name + ' ' + c.L_Name As Full_Name,
 	c.bio, c.ID As ContactID, c.profile_pic,
-	c.phone,
-	m.txt, m.files, m.music, m.video, m.Picture, m.locations, m.times, m.dates
+	c.phone
 From 
 	dbo.MSG As m Inner Join dbo.Contact As c 
 	On c.ID = m.Sender_ID
 
 
+-- Materialized View For Members
+Create Or Alter View Member_Contact
+With Schemabinding
+As
+Select 
+	c.F_Name + ' ' + c.L_Name As Full_Name,
+	c.bio, c.ID As ContactID, c.profile_pic,
+	c.phone
+From 
+	dbo.Members As m Inner Join dbo.Contact As c
+	On c.ID = m.member_id
 
---For Finding all the chats in a specific PV / Group / Channel
+
+-- For Finding all the chats in a specific PV / Group / Channel
 Create Or Alter Function Bring_Chat(@chatID Char(7))
 Returns Table
 As
 Return 
-	--view
-	Select *
+	Select
+		c.txt, c.files, c.music, c.video, c.Picture, c.locations, c.times, c.dates,
+		c.Full_Name,
+		c.bio, c.ContactID, c.profile_pic,
+		c.phone
 	From 
-	(Select MD.*, ROW_NUMBER() OVER(PARTITION BY m.Sender_ID ORDER BY m.Sender_ID DESC) As rn
-	From MSG As m, dbo.Message_Details As MD
-	Where 
-		m.Chat_ID = @chatID And
-		MD.ContactID = m.Sender_ID) As res
+		(Select MD.*, ROW_NUMBER() OVER(PARTITION BY m.Sender_ID ORDER BY m.Sender_ID DESC) As rn
+		From MSG As m, dbo.Message_Details As MD
+		Where 
+			m.Chat_ID = @chatID And
+			MD.ContactID = m.Sender_ID) As c
 	Where rn = 1;
 
 
@@ -41,10 +56,18 @@ Return
 Create Or Alter Function Members_Detail(@chatID Char(7))
 Returns Table
 As
-Return 
-	Select *
-	From Members As m
-	Where m.chat_id = @chatID;
+Return
+	Select
+		c.Full_Name,
+		c.bio, c.ContactID, c.profile_pic,
+		c.phone
+	From
+		(Select mc.*, ROW_NUMBER() OVER(PARTITION BY m.member_id ORDER BY m.member_id DESC) As rn
+		From Members As m, Member_Contact As mc
+		Where 
+			m.chat_id = @chatID And
+			mc.ContactID = m.member_id) As c
+	Where rn = 1;
 
 
 
@@ -52,13 +75,20 @@ CREATE OR ALTER PROCEDURE Open_Chat_Proc
 	@ChatID CHAR(7)
 AS
 BEGIN
+	-- Chat
 	Select *
-	From Bring_Chat(@ChatID) b
+	From Bring_Chat(@ChatID) As b
 	Order By b.dates, b.times Asc
 
+	-- Members
 	Select *
-	From Members As m
-	Where m.chat_id = @ChatID
+	From Members_Detail(@ChatID) As md
+	Order By md.Full_Name Asc
+
+	-- Setting
+	Select *
+	From
+
 END;
 
 --Drop Procedure Group_Channel_P
