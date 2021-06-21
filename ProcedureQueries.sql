f2e9f5830c156f23b07d359cd38a2c10443b875b
Iliya.mi <iliya.mi78@gmail.com>
2021-06-22 00:55:40 +0430

    Chat Procedure Done - 2 Materialized Views/1 View/3 Functions -

--- ProcedureQueries.sql
+++ ProcedureQueries.sql
@@ -10,10 +10,10 @@ Create Or Alter View Message_Details
 With Schemabinding
 As
 Select
-	m.txt, m.files, m.music, m.video, m.Picture, m.locations, m.times, m.dates,
 	c.F_Name + ' ' + c.L_Name As Full_Name,
 	c.bio, c.ID As ContactID, c.profile_pic,
-	c.phone
+	c.phone,
+	m.txt, m.files, m.music, m.video, m.Picture, m.locations, m.times, m.dates
 From 
 	dbo.MSG As m Inner Join dbo.Contact As c 
 	On c.ID = m.Sender_ID
@@ -32,16 +32,26 @@ From
 	On c.ID = m.member_id
 
 
+-- View For Admins 
+Create Or Alter View Admins_V
+As
+	Select h.Admin_ID, h.privilege, h.recentACC, h.A_ID
+	From 
+		dbo.Has_AA As h Inner Join dbo.Accessibility As a 
+		On a.A_ID = h.A_ID Inner Join Admin As ad
+		On ad.Contact_ID = h.Admin_ID;
+
+
 -- For Finding all the chats in a specific PV / Group / Channel
 Create Or Alter Function Bring_Chat(@chatID Char(7))
 Returns Table
 As
 Return 
 	Select
-		c.txt, c.files, c.music, c.video, c.Picture, c.locations, c.times, c.dates,
 		c.Full_Name,
 		c.bio, c.ContactID, c.profile_pic,
-		c.phone
+		c.phone,
+		c.txt, c.files, c.music, c.video, c.Picture, c.locations, c.times, c.dates
 	From 
 		(Select MD.*, ROW_NUMBER() OVER(PARTITION BY m.Sender_ID ORDER BY m.Sender_ID DESC) As rn
 		From MSG As m, dbo.Message_Details As MD
@@ -70,9 +80,43 @@ Return
 	Where rn = 1;
 
 
+-- Finding admins of a group or channel
+--Drop Function Find_Admin
+Create Or Alter Function Find_Admin(@Acc_ID Char(7))
+Returns Table
+As
+Return
+	Select
+		c.F_Name + ' ' + c.L_Name As Full_Name,
+		c.bio, c.ID As ContactID, c.profile_pic,
+		c.phone
+	From 
+		Admins_V As av Inner Join Contact As c
+		On c.ID = av.Admin_ID
+	Where av.A_ID = @Acc_ID;
+
+
+-- Find Accessibility of a chat form chat_id
+Create Or Alter Procedure Find_Acc 
+	@Chat_ID Char(7),
+	@Acc_ID Char(7)
+As
+Begin
+	
+		Select @Acc_ID =
+		Case
+			When g.Chat_ID = @Chat_ID And g.A_ID Is Not Null
+			Then g.A_ID
+			When ch.Chat_ID = @Chat_ID And ch.A_ID Is Not Null
+			Then ch.A_ID
+			End
+		From Groups As g, Channel As ch
+End;
+
 
 CREATE OR ALTER PROCEDURE Open_Chat_Proc
-	@ChatID CHAR(7)
+	@ChatID CHAR(7),
+	@AccID CHAR(7)
 AS
 BEGIN
 	-- Chat
@@ -85,14 +129,35 @@ BEGIN
 	From Members_Detail(@ChatID) As md
 	Order By md.Full_Name Asc
 
-	-- Setting
+	-- Accessibility
+	Select Distinct a.photo, a.Descriptions, a.inv_link As Link
+	From Accessibility As a, Groups As g
+	Where 
+		a.A_ID = g.A_ID And
+		a.A_ID = @AccID
+	Union
+	Select Distinct a.photo, a.Descriptions, a.inv_link As Link
+	From Accessibility As a, Channel As ch
+	Where 
+		a.A_ID = ch.A_ID And
+		a.A_ID = @AccID
+
+	-- Admin
 	Select *
-	From
+	From Find_Admin(@AccID)
 
 END;
 
 --Drop Procedure Group_Channel_P
 
-EXEC Open_Chat_Proc @ChatID = '1000000';
-EXEC Open_Chat_Proc @ChatID = '2000000';
+/* ======================= [Open Chat Procedure] ======================= 
+	1)Messages
+	2)Members
+	3)Accessibility (Channel/Group Profile Photo - Description - Join Link)
+	4)Owners And Admins
+*/
+-- A Channel Test
+EXEC Open_Chat_Proc @ChatID = '2000000', @AccID = '1111111';
+-- A PV Test
+EXEC Open_Chat_Proc @ChatID = '1000000', @AccID = '000none';
 
