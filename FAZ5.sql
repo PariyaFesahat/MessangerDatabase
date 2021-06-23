7342686b964f3f19f5001b483077ec67168d4444
Mahdi <amirmahdishadman98@gmail.com>
2021-06-23 15:59:14 +0430

    added trigger for not allowed delete or update message after 2 days

--- FAZ5.sql
+++ FAZ5.sql
@@ -51,3 +51,36 @@ EXEC ADD_CONTACT_FOR_USER @CONTACT_ID = '8512452',@USER_ID='1364758'
 
 
 --============================================================================================================================
+
+--trigger for not allow for delete or update after 2 days for messages.
+
+
+CREATE OR ALTER TRIGGER FORBID_DELETE_MESSAGE
+ON dbo.MSG
+FOR DELETE,UPDATE
+AS
+IF EXISTS (SELECT * FROM DELETED D WHERE DATEDIFF(day,d.dates, cast(GETDATE() as date))>2)
+BEGIN
+    RAISERROR('You can''t delete or edit your message after 2 days!!', 16, 1);
+	ROLLBACK
+END;
+
+--TEST
+
+select * from MSG m where m.MSG_ID='3336699'
+
+
+DELETE FROM MSG WHERE MSG_ID='3336699'
+
+
+UPDATE MSG
+SET txt='ASD'
+WHERE MSG_ID='3336699'
+
+INSERT INTO MSG
+VALUES
+('9541387','5648521','1364758','salam khobi?','14:06:10','2021-06-23',NULL,NULL,NULL,NULL,NULL);
+--9541387
+--3336699
+
+--============================================================================================================================
